9e8b8886365c9cb01effe51b3fcc5e46b61168af
krzhannikov <k.rzhannikov@gmail.com>
2021-01-13 11:44:15 +0200

    Tasks for Lession 01

---
+++ Task1(11.01.21).py
@@ -0,0 +1,14 @@
+# GeekBrains 'Профессия программист Python', Курс 01 'Основы языка Python',
+# Урок 01 'Знакомство с Python', Задача 1:
+# "Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя
+# несколько чисел и строк и сохраните в переменные, выведите на экран."
+
+name = 'John'
+surname = 'Stone'
+age = 55
+print('Name:', name, '| Surname:', surname, '| Age:', age)
+
+name = input('Print your name: ')
+surname = input('Print your surname: ')
+age = int(input('Print your age: '))
+print('Name:', name, '| Surname:', surname, '| Age:', age)
